1f5e77dc97247453bb84b4b50b56c80ef5e141ca
eve8888888 <eve8888888@vip.qq.com>
2018-11-06 22:08:06 +0800

    Commit

---
+++ ForeachLoop/src/foreachLoop/Test.java
@@ -0,0 +1,11 @@
+package foreachLoop;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+        for (int temp :
+                num) {
+            System.out.print(temp + ",");
+        }
+    }
+}
