49c853dd807dae693edbf08bde4bd92e2c1ac48b
eve8888888 <eve8888888@vip.qq.com>
2018-11-06 22:54:27 +0800

    Commit

---
+++ StaticImport/src/StaticImport/MyMath.java
@@ -0,0 +1,10 @@
+package StaticImport;
+
+public class MyMath {
+    public static int Add(int x,int y){
+        return x + y;
+    }
+    public static int Sub(int x,int y){
+        return x - y;
+    }
+}
---
+++ StaticImport/src/StaticImport/Test.java
@@ -0,0 +1,10 @@
+package StaticImport;
+
+import static   StaticImport.MyMath.*;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Add(1,2));
+        System.out.println(Sub(8,2));
+    }
+}
