716441e667e17a4ec0c9ecd27475b485de89dab7
eve8888888 <eve8888888@vip.qq.com>
2018-11-08 01:36:37 +0800

    commit

--- Jewels and Stones/src/JewelsandStones/Test.java
+++ Jewels and Stones/src/JewelsandStones/Test.java
@@ -1,5 +1,20 @@
 package JewelsandStones;
 
+/**
+ * 宝石与石头  Jewels and Stones
+ */
+
+/**
+ * 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。
+ * S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ *
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ */
+
+/**
+ * 输入: J = "aA", S = "aAAbbbb"
+ * 输出: 3
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println(Solution.numJewelsInStones("aA","aAAbbb"));
